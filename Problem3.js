be4581c4f945c811e280a22bfa835394c353e3c2
Sanjeet <Sanjeetsinghchahal933@gmail.com>
2023-08-30 15:38:23 +0530

    Correction in Problem3.js

--- Problem3.js
+++ Problem3.js
@@ -1,9 +1,9 @@
 // The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
-const model_arr = []
 function getSorted(inventory){
+   const modelArr = []
    for(let i = 0;i < inventory.length;i++){
-    model_arr.push(inventory[i].car_model)
+    modelArr.push(inventory[i].car_model)
    }
-   return model_arr.sort()
+   return modelArr.sort()
 }
 module.exports = getSorted
