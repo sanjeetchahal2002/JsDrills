860fc48d19f8e546fcb388a04429a33ac94ba5ce
Sanjeet <Sanjeetsinghchahal933@gmail.com>
2023-08-30 15:42:09 +0530

    Made change

--- Problem5.js
+++ Problem5.js
@@ -1,14 +1,14 @@
 // ==== Problem #5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
 
-let n = 0
 function getOldCars(res){
+    let oldCarCount = 0
     for(let i = 0; i< res.length;i++){
         if(res[i] < 2000){
-            n++
+            oldCarCount++
         }
     }
-    return n
+    return oldCarCount
 }
 
 module.exports = getOldCars
