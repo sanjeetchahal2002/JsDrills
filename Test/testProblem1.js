13b91912810f319837b68a011f5ead9177925ea1
Sanjeet <Sanjeetsinghchahal933@gmail.com>
2023-08-30 12:10:13 +0530

    Third Solution

--- Test/testProblem1.js
+++ Test/testProblem1.js
@@ -1,5 +1,5 @@
-let inventory = require('/home/sanjeet/JsDrills/inventory.js'); 
-const findCarById = require('/home/sanjeet/JsDrills/Problem1.js');
+let inventory = require('../inventory.js'); 
+const findCarById = require('../Problem1.js');
 const car = findCarById(inventory, 33);
 
 console.log(`Car ${car.id} is a ${car.car_year} ${car.car_make} ${car.car_model}`);
--- Test/testProblem2.js
+++ Test/testProblem2.js
@@ -1,5 +1,5 @@
-let inventory = require('/home/sanjeet/JsDrills/inventory.js')
-const LastCarId = require('/home/sanjeet/JsDrills/Problem2js')
+let inventory = require('../inventory.js')
+const LastCarId = require('../Problem2.js')
 const lastCar = LastCarId(inventory)
 
 console.log(`Last car is a ${lastCar.car_make} ${lastCar.car_model}`)
